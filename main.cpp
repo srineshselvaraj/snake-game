f63f2ac8ac6166b514a95798afdead1814136d69
srineshselvaraj <srinesh9@gmail.com>
2024-01-07 20:11:34 -0500

    Small update
    Got a small green square on the screen

--- main.cpp
+++ main.cpp
@@ -6,19 +6,22 @@ const int SCREEN_HEIGHT = 400;
 
 int main(int argc, char* args[]){
     SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
+    //SDL_Surface* screenSurface = NULL;
+    SDL_Renderer* renderer = NULL;
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     }
     else{
-        window = SDL_CreateWindow("Snake v1.9", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         if(window == NULL){
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         }
         else{
-            screenSurface = SDL_GetWindowSurface(window);
+            /*screenSurface = SDL_GetWindowSurface(window);
             SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
-            SDL_UpdateWindowSurface(window);
+            SDL_UpdateWindowSurface(window);*/
+
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
             SDL_Event e;
             bool quit = false;
@@ -28,6 +31,14 @@ int main(int argc, char* args[]){
                         quit = true;
                     }
                 }
+
+                SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+                SDL_RenderClear(renderer);
+
+                SDL_Rect snake = {0, 0, SCREEN_WIDTH / 20, SCREEN_HEIGHT / 20};
+                SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
+                SDL_RenderFillRect(renderer, &snake);
+                SDL_RenderPresent(renderer);
             }
         }
     }
